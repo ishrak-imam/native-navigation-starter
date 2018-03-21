2f2e7d3954b5d661758381c0a6d6d51e3bdadc22
Ishrak <ishrak.imam@gmail.com>
2018-03-21 16:15:56 +0600

    keyboard avoiding view used

--- src/modules/auth/index.js
+++ src/modules/auth/index.js
@@ -1,22 +1,108 @@
 import React, {Component} from 'react';
 import {
-  Screen, Text, View, Button
+  Screen, TextInput
 } from '@shoutem/ui';
+import {KeyboardAvoidingView} from 'react-native';
 
 export default class Auth extends Component {
   render () {
     return (
       <Screen styleName='paper'>
-        <View styleName='horizontal'>
-          <Button styleName='confirmation'>
-            <Text>REMOVE</Text>
-          </Button>
-
-          <Button styleName='confirmation secondary'>
-            <Text>UPDATE</Text>
-          </Button>
-        </View>
+        <KeyboardAvoidingView behavior='padding' style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <TextInput
+            placeholder={'Email'}
+            onChangeText={(text) => console.log(text)}
+          />
+          <TextInput
+            style={{marginTop: 10}}
+            placeholder={'Password'}
+            secureTextEntry
+            onChangeText={(text) => console.log(text)}
+          />
+        </KeyboardAvoidingView>
       </Screen>
     );
   }
 }
+
+// import React from 'react';
+// import {
+//   TouchableOpacity, View, Text,
+//   FlatList
+// } from 'react-native';
+
+// class MyListItem extends React.Component {
+//   constructor () {
+//     super();
+//     this._onPress = this._onPress.bind(this);
+//   }
+//   _onPress () {
+//     this.props.onPressItem(this.props.id);
+//   }
+
+//   shouldComponentUpdate (nextProps) {
+//     return nextProps.selected !== this.props.selected;
+//   }
+
+//   render () {
+//     const backgroundColor = `${this.props.selected ? 'limegreen' : 'white'}`;
+//     return (
+//       <TouchableOpacity onPress={this._onPress}>
+//         <View style={{height: 50, width: 200, padding: 10, borderWidth: 1, backgroundColor}}>
+//           <Text>
+//             {this.props.title}
+//           </Text>
+//         </View>
+//       </TouchableOpacity>
+//     );
+//   }
+// }
+
+// export default class MultiSelectList extends React.Component {
+//   constructor () {
+//     super();
+//     let data = [];
+//     for (let i = 1; i <= 100; i++) {
+//       data.push({id: i, title: `title for ${i}`});
+//     }
+//     this.state = {
+//       data,
+//       selected: 1
+//     };
+//     this._keyExtractor = this._keyExtractor.bind(this);
+//     this._onPressItem = this._onPressItem.bind(this);
+//     this._renderItem = this._renderItem.bind(this);
+//   }
+
+//   _keyExtractor (item) {
+//     return item.id.toString();
+//   }
+
+//   _onPressItem (id) {
+//     this.setState({selected: id});
+//   }
+
+//   _renderItem ({item}) {
+//     return (
+//       <MyListItem
+//         id={item.id}
+//         onPressItem={this._onPressItem}
+//         selected={item.id === this.state.selected}
+//         title={item.title}
+//       />
+//     );
+//   }
+
+//   render () {
+//     return (
+//       <View style={{paddingLeft: 20}}>
+//         <FlatList
+//           data={this.state.data}
+//           extraData={this.state}
+//           keyExtractor={this._keyExtractor}
+//           renderItem={this._renderItem}
+//         />
+//       </View>
+//     );
+//   }
+// }
--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,13 +1,22 @@
-
 import React, {Component} from 'react';
+import {Dimensions} from 'react-native';
 import {getTheme} from '@shoutem/ui';
 import {StyleProvider} from '@shoutem/theme';
+const {width} = Dimensions.get('window');
 const theme = getTheme();
+console.log('THEME ::: ', theme);
 const customTheme = {
   ...theme,
   'shoutem.ui.Button': {
     ...theme['shoutem.ui.Button'],
     borderColor: '#000000'
+  },
+  'shoutem.ui.TextInput': {
+    ...theme['shoutem.ui.TextInput'],
+    borderWidth: 1,
+    width: width - 100,
+    paddingVertical: 0,
+    height: 45
   }
 };
 
